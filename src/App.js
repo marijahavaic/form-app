989d96c9db8124dd73e0db385ba40830de9ede34
Marija Havaic <marija.havaic@gmail.com>
2022-05-11 18:54:39 +0200

    Validation and Displaying Errors
    -added errorMessages, patterns and required properties to inputs
    -defined focused, onBlur and onFocus attributes for validating inputs

--- src/App.js
+++ src/App.js
@@ -17,15 +17,21 @@ function App() {
             id: 1,
             name: "username",
             type: "text",
-            placeholder: "username",
+            placeholder: "Enter Username",
+            errorMessage:
+                "Username must be at 3-20 characters and shouldn't include any special character.",
             label: "Username",
+            pattern: "^[A-Za-z0-9]{3,20}$",
+            required: true,
         },
         {
             id: 2,
             name: "email",
             type: "email",
-            placeholder: "email",
+            placeholder: "Enter Email",
+            errorMessage: "Email is not valid",
             label: "Email",
+            required: true,
         },
         {
             id: 3,
@@ -33,20 +39,28 @@ function App() {
             type: "date",
             placeholder: "birthday",
             label: "Birthday",
+            required: true,
         },
         {
             id: 4,
             name: "password",
             type: "password",
-            placeholder: "password",
+            placeholder: "Enter Password",
+            errorMessage:
+                "Password must be 8-20 characters and include at least 1 letter, 1 number and 1 special character.",
             label: "Password",
+            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+            required: true,
         },
         {
             id: 5,
             name: "confirmPassword",
             type: "password",
             placeholder: "Confirm Password",
+            errorMessage: "Passwords don't match.",
             label: "Confirm Password",
+            pattern: values.password,
+            required: true,
         },
     ];
 
--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./FormInput.css";
 
 function FormInput(props) {
-    const { label, handleChange, id, ...inputProps } = props;
+    const [focused, setFocused] = useState(false);
+    const { label, handleChange, id, errorMessage, ...inputProps } = props;
+
+    const handleFocus = (e) => {
+        setFocused(true);
+    };
     return (
         <div className="FormInput">
             <label>{label}</label>
-            <input {...inputProps} onChange={handleChange} />
+            <input
+                {...inputProps}
+                onChange={handleChange}
+                onBlur={handleFocus}
+                onFocus={
+                    inputProps.name === "comfirmPassword" && setFocused(true)
+                }
+                focused={focused.toString()}
+            />
+            <span>{errorMessage}</span>
         </div>
     );
 }
