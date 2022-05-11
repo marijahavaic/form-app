90d7a6decdea2506313bbfb8ede537b2b78b8f16
Marija Havaic <marija.havaic@gmail.com>
2022-05-11 17:40:26 +0200

    Handle multiple form inputs
    - Created state object values with useState hook in which
    all of the form values are going to be saved.
    - Created an array of inputs that is mapped though and
    input props are passed to each FormInput component.
    -Created handleChange method to handle inputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,78 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
 import "./App.css";
 import FormInput from "./Components/FormInput";
 
 function App() {
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        birthday: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const inputs = [
+        {
+            id: 1,
+            name: "username",
+            type: "text",
+            placeholder: "username",
+            label: "Username",
+        },
+        {
+            id: 2,
+            name: "email",
+            type: "email",
+            placeholder: "email",
+            label: "Email",
+        },
+        {
+            id: 3,
+            name: "birthday",
+            type: "date",
+            placeholder: "birthday",
+            label: "Birthday",
+        },
+        {
+            id: 4,
+            name: "password",
+            type: "password",
+            placeholder: "password",
+            label: "Password",
+        },
+        {
+            id: 5,
+            name: "confirmPassword",
+            type: "password",
+            placeholder: "Confirm Password",
+            label: "Confirm Password",
+        },
+    ];
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    console.log(values);
+
     return (
         <div className="App">
-            <form>
-                <FormInput placeholder="Username" />
-                <FormInput placeholder="Email" />
-                <FormInput placeholder="Full" />
+            <form onSubmit={handleSubmit}>
+                {inputs.map((input) => (
+                    <FormInput
+                        key={input.id}
+                        {...input}
+                        value={values[input.name]}
+                        handleChange={handleChange}
+                    />
+                ))}
+
+                <button>Submit</button>
             </form>
         </div>
     );
--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -3,10 +3,11 @@ import React from "react";
 import "./FormInput.css";
 
 function FormInput(props) {
+    const { label, handleChange, id, ...inputProps } = props;
     return (
         <div className="FormInput">
-            <lable>Username</lable>
-            <input placeholder={props.placeholder} />
+            <label>{label}</label>
+            <input {...inputProps} onChange={handleChange} />
         </div>
     );
 }
